c925a443778bf96f45c4f5ae3500748f852bac00
leandro-miranda <leandromiranda2010@gmail.com>
2023-03-10 09:01:04 -0300

    refactory

--- ui-twitter/src/pages/Timeline.tsx
+++ ui-twitter/src/pages/Timeline.tsx
@@ -11,24 +11,24 @@ const tweets = [
 ];
 
 export function Timeline() {
-	return (
-		<main className='timeline'>
-          <Header title='Home' />
+  return (
+    <main className='timeline'>
+      <Header title='Home' />
 
-          <form className='new-tweet-form'>
-            <label htmlFor="tweet">
-              <img src="https://github.com/leandro-miranda.png" alt="Leandro Miranda" />
-              <textarea id="tweet" placeholder="What's happening?" />
-            </label>
+      <form className='new-tweet-form'>
+        <label htmlFor="tweet">
+          <img src="https://github.com/leandro-miranda.png" alt="Leandro Miranda" />
+          <textarea id="tweet" placeholder="What's happening?" />
+        </label>
 
-            <button type='submit'>Tweet</button>
-          </form>
+        <button type='submit'>Tweet</button>
+      </form>
 
-          <Separator />
+      <Separator />
 
-          {tweets.map(tweet => {
-            return <Tweet key={tweet} content={tweet} />
-          })}
-        </main>
-	)
+      {tweets.map(tweet => {
+        return <Tweet key={tweet} content={tweet} />
+      })}
+    </main>
+  )
 }
