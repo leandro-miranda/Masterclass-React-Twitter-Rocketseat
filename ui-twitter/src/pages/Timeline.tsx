978a5fe271a45f5dfa8ca56a5b5f2ecc2ab4d290
leandro-miranda <leandromiranda2010@gmail.com>
2023-03-10 09:55:50 -0300

    saving new tweets

--- ui-twitter/src/pages/Timeline.tsx
+++ ui-twitter/src/pages/Timeline.tsx
@@ -1,24 +1,39 @@
+import { FormEvent, useState } from 'react';
 import { Header } from '../components/Header';
 import { Tweet } from '../components/Tweet';
 import { Separator } from '../components/Separator';
 
 import './Timeline.css';
 
-const tweets = [
-  'Meu primeiro tweet',
-  'Teste',
-  'Deu certo tweetar!'
-];
+let newTweet = ''
 
 export function Timeline() {
+  const [tweets, setTweets] = useState([
+    'Meu primeiro tweet',
+    'Teste',
+    'Deu certo tweetar!'
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets])
+  }
+
   return (
     <main className='timeline'>
       <Header title='Home' />
 
-      <form className='new-tweet-form'>
+      <form onSubmit={createNewTweet} className='new-tweet-form'>
         <label htmlFor="tweet">
           <img src="https://github.com/leandro-miranda.png" alt="Leandro Miranda" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            onChange={(event) => {
+              newTweet = event.target.value
+            }}
+          />
         </label>
 
         <button type='submit'>Tweet</button>
